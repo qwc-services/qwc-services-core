d5ae5e18d2f29d7cfcc06f9309cee1da39b56162
Mathias Walker <mwa@sourcepole.ch>
2018-09-26 17:26:34 +0200

    add resource_permissions() and resource_restrictions() to PermissionClient for generic queries of permitted or restricted resources of any resource type

--- qwc_services_core/permission.py
+++ qwc_services_core/permission.py
@@ -18,7 +18,7 @@ class PermissionClient():
         """Constructor"""
         # get permission service URL from ENV (default: local service)
         self.service_url = os.environ.get(
-            'CONFIG_SERVICE_URL', 'http://localhost:5010/')
+            'CONFIG_SERVICE_URL', 'http://localhost:5010/').rstrip('/') + '/'
         # check if cache is valid every x seconds (default: 60s)
         self.config_check_interval = os.environ.get(
             'CONFIG_CHECK_INTERVAL', 60)
@@ -34,6 +34,92 @@ class PermissionClient():
         self.last_update_check = None
         self.last_cache_flush = None
 
+    def resource_permissions(self, resource_type, params, username,
+                             cache_duration=86400):
+        """Return permitted resources for a resource type.
+
+        :param str resource_type: Resource type
+        :param obj params: Optional request parameters with
+                           name=<name filter>&parent_id=<parent filter>
+        :param str username: User name
+        :param int cache_duration: Time in seconds until expiry (default: 24h)
+        """
+        permissions = None
+
+        cache_key = "permitted_%s" % resource_type
+
+        if cache_duration:  # Caching active
+            # check if cache is still valid
+            self.check_cache()
+
+            # get permissions from cache
+            permissions = self.cache.read(
+                cache_key, username, params.values())
+            if permissions:
+                return permissions
+
+        # build request URL: http://<service_url>/permissions/<resource_type>
+        url = urljoin(self.service_url, 'permissions/%s' % resource_type)
+
+        reqparams = params.copy()  # don't change params before cache.write
+        if username:
+            reqparams.update({'username': username})
+
+        # send request to permission service
+        response = requests.get(url, headers=self.headers, params=reqparams,
+                                timeout=30)
+        if response.status_code == requests.codes.ok:
+            permissions = response.json()['permissions']
+
+        if cache_duration:  # Caching active
+            self.cache.write(cache_key, username, params.values(),
+                             permissions, cache_duration)
+
+        return permissions
+
+    def resource_restrictions(self, resource_type, params, username,
+                              cache_duration=86400):
+        """Return restricted resources for a resource type.
+
+        :param str resource_type: Resource type
+        :param obj params: Optional request parameters with
+                           name=<name filter>&parent_id=<parent filter>
+        :param str username: User name
+        :param int cache_duration: Time in seconds until expiry (default: 24h)
+        """
+        restrictions = None
+
+        cache_key = "restricted_%s" % resource_type
+
+        if cache_duration:  # Caching active
+            # check if cache is still valid
+            self.check_cache()
+
+            # get restrictions from cache
+            restrictions = self.cache.read(
+                cache_key, username, params.values())
+            if restrictions:
+                return restrictions
+
+        # build request URL: http://<service_url>/restrictions/<resource_type>
+        url = urljoin(self.service_url, 'restrictions/%s' % resource_type)
+
+        reqparams = params.copy()  # don't change params before cache.write
+        if username:
+            reqparams.update({'username': username})
+
+        # send request to permission service
+        response = requests.get(url, headers=self.headers, params=reqparams,
+                                timeout=30)
+        if response.status_code == requests.codes.ok:
+            restrictions = response.json()['restrictions']
+
+        if cache_duration:  # Caching active
+            self.cache.write(cache_key, username, params.values(),
+                             restrictions, cache_duration)
+
+        return restrictions
+
     def service_permissions(self, service, params, username,
                             cache_duration=86400):
         """Return service permissions if available and permitted.
@@ -46,24 +132,7 @@ class PermissionClient():
         permissions = None
         if cache_duration:  # Caching active
             # check if cache is still valid
-            if (self.last_update_check is None
-                or self.last_update_check +
-                    self.config_check_interval < time.time()):
-                # get last permissions update from permission service
-                url = urljoin(self.service_url, 'last_update')
-                response = requests.get(url, headers=self.headers, timeout=30)
-                if response.status_code == requests.codes.ok:
-                    permissions_updated_at = datetime.strptime(
-                        response.json()['permissions_updated_at'],
-                        "%Y-%m-%d %H:%M:%S"
-                    )
-                    if (self.last_cache_flush is None
-                            or self.last_cache_flush < permissions_updated_at):
-                        # flush cache if obsolete
-                        self.cache.init()
-                        self.last_cache_flush = permissions_updated_at
-
-                self.last_update_check = time.time()
+            self.check_cache()
 
             # get permissions from cache
             permissions = self.cache.read(
@@ -165,3 +234,24 @@ class PermissionClient():
             'search', {'dataset': dataset}, username,
             self.default_cache_duration
         )
+
+    def check_cache(self):
+        """Check if cache is still valid and flush if obsolete."""
+        if (self.last_update_check is None
+            or self.last_update_check +
+                self.config_check_interval < time.time()):
+            # get last permissions update from permission service
+            url = urljoin(self.service_url, 'last_update')
+            response = requests.get(url, headers=self.headers, timeout=30)
+            if response.status_code == requests.codes.ok:
+                permissions_updated_at = datetime.strptime(
+                    response.json()['permissions_updated_at'],
+                    "%Y-%m-%d %H:%M:%S"
+                )
+                if (self.last_cache_flush is None
+                        or self.last_cache_flush < permissions_updated_at):
+                    # flush cache if obsolete
+                    self.cache.init()
+                    self.last_cache_flush = permissions_updated_at
+
+            self.last_update_check = time.time()
