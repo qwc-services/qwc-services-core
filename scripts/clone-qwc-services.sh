72b42d59b9b2f527f06f52ccaa6308966dcf9b2d
Pirmin Kalberer <pka@sourcepole.ch>
2018-12-12 18:12:57 +0100

    Add Git helper scripts

---
+++ scripts/clone-qwc-services.sh
@@ -0,0 +1,21 @@
+GITBASE=https://github.com/qwc-services
+#GITBASE=git@github.com:qwc-services
+REPOS="qwc-config-service 
+       qwc-docker
+       qwc-ogc-service
+       qwc-services-core
+       qwc-map-viewer
+       qwc-config-db
+       qwc-legend-service
+       qwc-admin-gui
+       qwc-db-auth
+       qwc-data-service
+       qwc-permalink-service
+       qwc-print-service
+       qwc-ldap-auth"
+
+for repo in $REPOS; do
+    if [ ! -d "${repo}" ]; then
+        git clone $GITBASE/$repo.git
+    fi
+done
