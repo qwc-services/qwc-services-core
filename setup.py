21d6ce7187b499fd7594a3d1364dbb5e7df2ae9a
Pirmin Kalberer <pka@sourcepole.ch>
2020-04-21 15:08:27 +0200

    Add LICENSE and update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
-from distutils.core import setup
-
+import setuptools
 
 desc = """\
 =================
 QWC Services Core
 =================
-
 Shared modules for QWC services.
 """
 
-setup(name='qwc-services-core',
-      version='0.1',
-      description='QWC Services Core',
-      long_description=desc,
-      author='Sourcepole AG',
-      author_email='info@sourcepole.ch',
-      url='https://github.com/qwc-services/qwc-services-core',
-      packages=['qwc_services_core'],
-      license='BSD-3-Clause'
-      )
+setuptools.setup(
+    name="qwc-services-core",
+    version="0.1.0",
+    author="Sourcepole AG",
+    author_email="info@sourcepole.ch",
+    description="Shared modules for QWC services",
+    long_description=desc,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/qwc-services/qwc-services-core",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
