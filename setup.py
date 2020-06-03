d55444b6a33a03a8f9b9c7fbbec8b547f60459ad
Pirmin Kalberer <pka@sourcepole.ch>
2020-06-03 21:37:47 +0200

    Release 0.9.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ Shared modules for QWC services.
 
 setuptools.setup(
     name="qwc-services-core",
-    version="0.9.0",
+    version="0.9.1",
     author="Sourcepole AG",
     author_email="info@sourcepole.ch",
     description="Shared modules for QWC services",
