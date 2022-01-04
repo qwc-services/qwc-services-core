b04136d073f7cc1fed84ef0b2f771042a282fee7
Pirmin Kalberer <pka@sourcepole.ch>
2022-01-04 14:17:21 +0100

    Bump version to 1.3.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ Shared modules for QWC services.
 
 setuptools.setup(
     name="qwc-services-core",
-    version="1.2.7",
+    version="1.3.0",
     author="Sourcepole AG",
     author_email="info@sourcepole.ch",
     description="Shared modules for QWC services",
