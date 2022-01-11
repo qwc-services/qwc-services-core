0f06a880dd84fb6206920c8948c69bc13610f7c4
Sandro Mani <manisandro@gmail.com>
2022-01-11 14:43:24 +0100

    Bump version to 1.3.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ Shared modules for QWC services.
 
 setuptools.setup(
     name="qwc-services-core",
-    version="1.3.0",
+    version="1.3.1",
     author="Sourcepole AG",
     author_email="info@sourcepole.ch",
     description="Shared modules for QWC services",
