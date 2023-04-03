82c5501e4ef8ab4c855154f278df63ca52044895
Pirmin Kalberer <pka@sourcepole.ch>
2023-04-03 10:55:16 +0200

    Set version to 1.3.16

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ Shared modules for QWC services.
 
 setuptools.setup(
     name="qwc-services-core",
-    version="1.3.15",
+    version="1.3.16",
     author="Sourcepole AG",
     author_email="info@sourcepole.ch",
     description="Shared modules for QWC services",
