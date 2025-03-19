2beb6e9d35efac89e7d31636798cf6afc5aac1e0
Sandro Mani <manisandro@gmail.com>
2025-03-19 10:07:52 +0100

    Set version to 1.4.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ Shared modules for QWC services.
 
 setuptools.setup(
     name="qwc-services-core",
-    version="1.4.3",
+    version="1.4.4",
     author="Sourcepole AG",
     author_email="info@sourcepole.ch",
     description="Shared modules for QWC services",
